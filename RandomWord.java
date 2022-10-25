c9cd4304130e51df28132dbe1e237c71de956a67
Rupa <110527772+SIRAPURUPA@users.noreply.github.com>
2022-10-25 23:07:48 +0530

    Update RandomWord.java

--- RandomWord.java
+++ RandomWord.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
-class RandomWord{
+public class RandomWord{
 
     public static void main(String[] args)
     {
